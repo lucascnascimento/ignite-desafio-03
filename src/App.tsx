eb319bbf339b5c15c7a35de28fb66766e0f5e0f9
Lucas Cordeiro <lucas.cordeiro@smarttbotter.com>
2023-03-25 14:50:17 -0300

    feat: set up base theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
+import "./index.css";
+
 function App() {
-  return <div>Hello World</div>;
+  return <div className="text-blue">Hello World</div>;
 }
 
 export default App;
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,22 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      blue: "#3294F8",
+      "base-title": "#E7EDF4",
+      "base-subtitle": "#C4D4E3",
+      "base-text": "#AFC2D4",
+      "base-span": "#7B96B2",
+      "base-label": "#3A536B",
+      "base-border": "#1C2F41",
+      "base-post": "#112131",
+      "base-profile": "#0B1B2B",
+      "base-background": "#071422",
+      "base-input": "#071422",
+    },
+    fontFamily: {
+      sans: ["Nunito", "sans-serif"],
+    },
     extend: {},
   },
   plugins: [],
